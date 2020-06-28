61a5f83dddabb25f946f78bca18fdd1b80d70046
BostonovOH <BostonovOH@bnipi.rosneft.ru>
2020-06-28 13:25:50 +0500

    sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,41 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'lowerAsc') {
+export function sortStrings(arr, param = 'asc') {
+  console.log(arr);
+  console.log(param);
 
+  let sorter1 = function (a, b) {
+      let x = a.toLowerCase();
+      let y = b.toLowerCase();
+      x = x.replace('ё', 'е');
+      y = y.replace('ё', 'е');
+
+      if (x > y) {
+          return 1;
+      }
+      if (y > x) {
+          return -1;
+      }
+
+      a.codePointAt(0) > b.codePointAt(0)
+      {
+        return -1
+      }
+
+      return 0;
+    };
+
+  switch (param) {
+    case 'asc':
+      arr.sort(sorter1);
+      break;
+    case 'desc':
+        arr.sort(sorter1);
+        arr.reverse();
+        break;
+    default:
+  }
+
+  return arr.slice();
 }
